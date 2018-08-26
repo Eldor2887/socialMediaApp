9c525d86af1e634f7410e9c70c100c246309a53e
Eldor Togaymurotov <eldorcodes@gmail.com>
2018-08-26 16:59:58 -0700

    deployment

---
+++ config/keys-prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MongoURI: 'mongodb://eldor:eldor01@ds233212.mlab.com:33212/socialmedia-prod',
+    GoogleClientID: '167045355588-evta15laingqal32cvtgjfmnfuqengbg.apps.googleusercontent.com',
+    GoogleClientSecret: 'st5fLLdN54-P7XqMuAwviPVQ'
+}
