6b8463bcc1f604bdbb3c0920b5c507dd3dd272fc
Eldor Togaymurotov <eldorcodes@gmail.com>
2018-08-26 17:33:36 -0700

    config keys added

--- config/keys-prod.js
+++ config/keys-prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MongoURI: 'mongodb://eldor:eldor01@ds233212.mlab.com:33212/socialmedia-prod',
-    GoogleClientID: '167045355588-evta15laingqal32cvtgjfmnfuqengbg.apps.googleusercontent.com',
-    GoogleClientSecret: 'st5fLLdN54-P7XqMuAwviPVQ'
+    MongoURI: process.env.MONGO_URI,
+    GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+    GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 }
